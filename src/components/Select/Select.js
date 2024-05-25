71b1bc9bf219a1c3d231e383b5715d2fd14eb4a4
Osita Ezeigbo <osiclin@yahoo.com>
2024-05-25 09:02:35 +0100

    Create select component

---
+++ src/components/Select/Select.js
@@ -0,0 +1,19 @@
+export default function Select({ title, containerClass, options, onChange }) {
+    return (
+        <div className={containerClass}>
+            <label htmlFor={title} className="text-center block">{title}</label>
+            <select
+                id={title}
+                name={title}
+                className={`h-[40px] px-[8px] bg-[#F9F9FB] text-base w-fit outline-0 border border-[#E3E5EB] font-normal rounded-[8px]`}
+                onChange={onChange}
+            >
+                <option value="">--select--</option>
+                <option value="">all</option>
+                {options.map(option => (
+                    <option key={option} value={option}>{option}</option>
+                ))}
+            </select>
+        </div>
+    )
+}
